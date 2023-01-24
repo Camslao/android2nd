bebaaed59a4ba8dd23efe92cb6401bb07816424a
Camslao <123285605+Camslao@users.noreply.github.com>
2023-01-24 17:53:32 +0800

    new

---
+++ app/src/main/java/com/example/androidshopapp/ResigsterActivity.java
@@ -0,0 +1,14 @@
+package com.example.androidshopapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResigsterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resigster);
+    }
+}
